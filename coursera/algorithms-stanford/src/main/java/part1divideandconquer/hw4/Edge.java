cd9fe4634ee8b9368299d82e239c87608aa5fd0f
Eric Fesenmaier <efesenma@cisco.com>
2019-05-11 14:14:36 -0700

    Restructured packages for part2

---
+++ coursera/algorithms-stanford/src/main/java/part1divideandconquer/hw4/Edge.java
@@ -0,0 +1,51 @@
+package part1divideandconquer.hw4;
+
+import java.util.Objects;
+
+public class Edge {
+    private Integer u;
+    private Integer v;
+
+    public Edge(Integer u, Integer v) {
+        this.u = u;
+        this.v = v;
+    }
+
+    public Integer getU() {
+        return u;
+    }
+
+    public Integer getV() {
+        return v;
+    }
+
+    public Edge replace(Integer oldId, Integer newId) {
+        if (u.equals(oldId)) {
+            u = newId;
+        }
+        if (v.equals(oldId)) {
+            v = newId;
+        }
+        return this;
+    }
+
+    public boolean isLoop() {
+        return u.equals(v);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(u, v);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Edge edge = (Edge)obj;
+        return (Objects.equals(u, edge.u) && Objects.equals(v, edge.v)) ||
+                (Objects.equals(u, edge.v) && Objects.equals(v, edge.u));
+    }
+}
---
+++ coursera/algorithms-stanford/src/main/java/part2graphs/hw1/KosarajuStronglyConnectedComponents.java
@@ -0,0 +1,4 @@
+package part2graphs.hw1;
+
+public class KosarajuStronglyConnectedComponents {
+}
